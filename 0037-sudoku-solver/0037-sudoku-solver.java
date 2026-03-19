0309a236531491f7818ff42d9a72c488ae340c53
Sourav Jaswal <159280970+souravjaswall@users.noreply.github.com>
2026-03-19 18:37:55 +0530

    Time: 391 ms (6.42%), Space: 43 MB (47.84%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -10,29 +10,24 @@ class Solution {
                     for (char c = '1'; c <= '9'; c++) {
                         if (isValid(board, row, col, c)) {
                             board[row][col] = c;
-
                             if (solve(board)) {
                                 return true;
                             } else {
-                                // Backtrack
                                 board[row][col] = '.';
                             }
                         }
                     }
-                    return false; // No valid number found for this cell
+                    return false;
                 }
             }
         }
-        return true; // All cells filled
+        return true;
     }
 
     private boolean isValid(char[][] board, int row, int col, char c) {
         for (int i = 0; i < 9; i++) {
-            // Check row
             if (board[i][col] == c) return false;
-            // Check column
             if (board[row][i] == c) return false;
-            // Check 3x3 box
             if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
         }
         return true;
